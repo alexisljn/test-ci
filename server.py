ce1664463a504a777f0a16f2263114a49487e02d
alexisljn <alexis.lejeune@gmx.fr>
2021-04-01 13:39:32 +0200

    Ca passe encore

--- server.py
+++ server.py
@@ -8,5 +8,10 @@ def index():
     """This module does blah blah."""
     return "Hello Porld !"
 
+@app.route('/<name_param>')
+def name(name_param):
+    """This function returns Hello <name_param>"""
+    return "Hello {0}".format(name_param)
+
 if __name__ == "__main__":
     app.run()
